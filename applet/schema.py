997f9dbb28c4e87e6015b1a32b18c8bc6e2263a8
shubin123 <44554264+Shubin123@users.noreply.github.com>
2022-07-05 05:59:10 -0400

    mutations added
    registration will send email that contains jwt

--- applet/schema.py
+++ applet/schema.py
@@ -8,17 +8,22 @@ import graphene
 from graphql_auth.schema import UserQuery
 from graphql_auth import mutations
 
-# class AuthMutation(graphene.ObjectType):
-#    register = mutations.Register.Field()
+class AuthMutation(graphene.ObjectType):
+    register = mutations.Register.Field()
+    verify_account = mutations.VerifyAccount.Field()
+    token_auth = mutations.ObtainJSONWebToken.Field()
+    update_account = mutations.UpdateAccount.Field()
+    password_reset = mutations.PasswordReset.Field()
+
 
 class Query(UserQuery, graphene.ObjectType):
     pass
 
-# class Mutation(AuthMutation,
-# # super.Mutation,
-# graphene.ObjectType):
-#    pass
+class Mutation(AuthMutation,
+# super.Mutation,
+graphene.ObjectType):
+   pass
 
 schema = graphene.Schema(query=Query,
-                         # mutation=Mutation
+                         mutation=Mutation
 )
