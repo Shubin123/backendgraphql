5bc6388771c28fa5ff72c8d27d7af879d28f967c
shubin123 <44554264+Shubin123@users.noreply.github.com>
2022-07-10 16:50:05 -0400

    add graphql subdomain
    subdomain change in settings for testing

--- applet/settings.py
+++ applet/settings.py
@@ -19,7 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["authoreteh.herokuapp.com", "127.0.0.1", "127.0.0.1:8000",
+ALLOWED_HOSTS = ["authoreteh.herokuapp.com",
+                 "authoreteh.herokuapp.com/graphql" ,
+                 "127.0.0.1", "127.0.0.1:8000",
                  "127.0.0.1:8000/graphql", ]
 
 # Application definition
